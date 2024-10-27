f6816be5d820d6d56aced5a7f00de90c0d7b3e18
Juanzan <hunterleaguecanal@gmail.com>
2024-10-27 17:52:08 -0500

    added sidebar functionality: now the sidebar button is displayed correctly and it provides button options to move between pages to ensure compatibility with mobile devices.

--- main.py
+++ main.py
@@ -5,25 +5,49 @@ import matplotlib as mt
 import pages as pg
 from styles.nav_bar_styles import styles
 
-
+# Set up the page configuration
 st.set_page_config(initial_sidebar_state="collapsed",
                    layout="wide",
                    page_title="Discover Spending Tracker",
-                   page_icon="images/top_logo.svg",
+                   page_icon="images/top_logo.svg"
                    )
 
+# Top navigation bar setup
+page = st_navbar(
+    ["Dashboard", "Spending Score", "Spending Tracker", "About Us"],
+    styles=styles, logo_page="Dashboard", logo_path="images/logo_white.svg",
+    options={"show_menu": True, "show_sidebar": True, "hide_nav": True}
+)
+
+# Load the custom HTML styling
 with open("styles/source.html", "r") as file:
     html_style = file.read()
 st.html(html_style)
 
-page = st_navbar(["Dashboard", "Spending Score", "Spending Tracker", "About Us"],
-                 styles=styles, logo_page="Dashboard", logo_path="images/logo_white.svg")
+# Sidebar navigation using buttons
 
-if page == "Dashboard":
+st.sidebar.image("images/logo_white.svg", width=None)
+with st.sidebar:
+    dashboard_button = st.button("Dashboard",)
+    spending_score_button = st.button("Spending Score")
+    spending_tracker_button = st.button("Spending Tracker")
+    about_us_button = st.button("About Us")
+
+if dashboard_button:
+    page = "Dashboard"
+elif spending_score_button:
+    page = "Spending Score"
+elif spending_tracker_button:
+    page = "Spending Tracker"
+elif about_us_button:
+    page = "About Us"
+
+# Display the content based on either the navbar or sidebar button click
+if page == "Dashboard" or dashboard_button:
     pg.dashboard()
-if page == "Spending Score":
+elif page == "Spending Score" or spending_score_button:
     pg.spending_score()
-if page == "Spending Tracker":
+elif page == "Spending Tracker" or spending_tracker_button:
     pg.spending_tracker()
-if page == "About Us":
+elif page == "About Us" or about_us_button:
     pg.about_us()
--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 def dashboard():
     st.title("Dashboard")
+    with st.sidebar:
+        st.write("Welcome to the your Dashboard!")
