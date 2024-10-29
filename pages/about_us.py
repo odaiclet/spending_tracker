e70a0c0a3d55c2893237c802f01e70e02c149220
LawrenceNolan <nlawr1215@icloud.com>
2024-10-29 16:30:59 -0500

    Finishing up changes for Aboutus

--- pages/about_us.py
+++ pages/about_us.py
@@ -2,21 +2,22 @@ import streamlit as st
 from streamlit_navigation_bar import st_navbar
 import pandas as pd
 import matplotlib as mt
-from .misc.descriptions import personalDescription
+from .misc.descriptions import *
 
 def about_us():
     
     st.title("About Us")
+    st.write("---") #divider
 
     col1, col2, col3, = st.columns(3)
 
     with st.container():
-        
         col1.image("images/Placeholder_Profile_Pic.svg", use_column_width=True)
-        col1.markdown(f"<div style='text-align: center;'>{personalDescription}</div>", unsafe_allow_html=True)
         col2.image("images/Placeholder_Profile_Pic.svg", use_column_width=True)
-        col2.markdown(f"<div style='text-align: center;'>{personalDescription}</div>", unsafe_allow_html=True)
         col3.image("images/Placeholder_Profile_Pic.svg", use_column_width=True)
-        col3.markdown(f"<div style='text-align: center;'>{personalDescription}</div>", unsafe_allow_html=True)
+        col1.markdown(f"<div style='text-align: center;'>{nolanDescription}</div>", unsafe_allow_html=True)
+        col2.markdown(f"<div style='text-align: center;'>{juanDescription}</div>", unsafe_allow_html=True)
+        col3.markdown(f"<div style='text-align: center;'>{destinyDescription}</div>", unsafe_allow_html=True)
 
-    # col1.write("bottomDesc") #placeholder
+    st.write("---")
+    st.markdown(f"<p style='text-align: center;'>{overallDescription}</p>", unsafe_allow_html=True)
--- pages/misc/descriptions.py
+++ pages/misc/descriptions.py
@@ -1,9 +1,25 @@
 overallDescription = """[Overall Descriptions]"""
 
-personalDescription = """
-<h3>Name</h3>
+juanDescription = """
+<h3>Juan Zanguitu</h3>
 
 Email
+
+Description
+"""
+
+destinyDescription = """
+<h3>Destiny Medina</h3>
+
+dmedina8@hawk.iit.edu
+
+Description
+"""
+
+nolanDescription = """
+<h3>Nolan Lawrence</h3>
+
+nlawrence1@hawk.iit.edu
 
 Description
 """
